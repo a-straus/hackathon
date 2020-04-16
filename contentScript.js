ff15bb06d5c9a0168968371b5888ada9439bc372
AP <ap@Allisons-MBP.cable.rcn.com>
2020-04-16 13:41:34 -0400

    added comment field

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,22 @@
 function comment() {
     let menu = document.createElement("div");
     menu.innerHTML = (`<div id="comment-menu">
-        <input id="comment-field"></input>
-        <button>Add comment</button>
-        console.log(document)
+        <textarea id="comment-field"></textarea>
+        <button id="cmt-btn">Add comment</button>
     </div>`);
     document.body.appendChild(menu);
+    document.getElementById('cmt-btn').addEventListener('click', function() {
+        let comment = this.previousSibling.previousSibling.value;
+        console.log(comment);
+        document.getElementById('comment-menu').remove();
+    });
 }
-comment();
+document.body.addEventListener('contextmenu', comment);
+// document.addEventListener('mouseup', function() {
+//     let highlighted = document.getSelection();
+//     console.log(highlighted);
+//     highlighted.focusNode.style.backgroundColor = 'yellowgreen';
+// });
+
 
 console.log(document)
