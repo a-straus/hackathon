50d76a072d3a8bffa54ab4865fb23a8e8390d54b
a-straus <A.Straus1@gmail.com>
2020-04-16 11:52:01 -0400

    got the icon back

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function () {
   chrome.contextMenus.create({
     id: 'ContextMenu',
     title: 'Context Menu',
-    contexts: ['selection']
+    contexts: ['all']
   });
 });
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
@@ -21,48 +21,50 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
   ]);
 });
 
-
 function commentsPopup() {
-    chrome.tabs.executeScript({
-        code: 'console.log(document)'
-        // code: 'comment();'
-        // file: 'contentScript.js',
-        // code: `let menu = document.createElement("div");
-        // menu.innerHTML = ('<div id="comment-menu">
-        //     <input id="comment-field"></input>
-        //     <button>Add comment</button>
-        // </div>');
-        // document.body.appendChild(menu);
-        // console.log(document)`
-      });
+  chrome.tabs.executeScript({
+    code: 'console.log(document)'
+    // code: 'comment();'
+    // file: 'contentScript.js',
+    // code: `let menu = document.createElement("div");
+    // menu.innerHTML = ('<div id="comment-menu">
+    //     <input id="comment-field"></input>
+    //     <button>Add comment</button>
+    // </div>');
+    // document.body.appendChild(menu);
+    // console.log(document)`
+  });
 
-    let comment = prompt('enter a comment');
+  let comment = prompt('enter a comment');
 }
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-        "id": "ContextMenu",
-        "title": "Annotate",
-        "contexts": ["all"],
-      });
+// chrome.runtime.onInstalled.addListener(function() {
+//     chrome.contextMenus.create({
+//         "id": "ContextMenu",
+//         "title": "Annotate",
+//         "contexts": ["all"],
+//       });
+// });
+
+chrome.contextMenus.onClicked.addListener(function () {
+  commentsPopup();
 });
 
-chrome.contextMenus.onClicked.addListener(function() {
-    commentsPopup();
-});
-
-chrome.runtime.onMessage.addListener(function() {
-chrome.tabs.executeScript({
+chrome.runtime.onMessage.addListener(function () {
+  chrome.tabs.executeScript({
     file: 'contentScript.js'
   });
 });
 
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+  chrome.declarativeContent.onPageChanged.addRules([
+    {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: 'developer.chrome.com' }
+        })
       ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }
+  ]);
+});
