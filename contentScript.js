7985bdd1cafee2d97abe083df55ecd4e429cfc87
a-straus <A.Straus1@gmail.com>
2020-04-16 15:24:33 -0400

    database in contentscript.js

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,24 @@
-// var db = new PouchDB('http://134.122.118.232:5984/hackathon');
+var db = new PouchDB('http://134.122.118.232:5984/hackathon');
+console.log('in contentScript', db);
 
 // console.log(db);
 
 function comment() {
-    let menu = document.createElement("div");
-    menu.innerHTML = (`<div id="comment-menu">
+  let menu = document.createElement('div');
+  menu.innerHTML = `<div id="comment-menu">
         <textarea id="comment-field"></textarea>
         <button id="cmt-btn">Add comment</button>
-    </div>`); 
-    document.body.appendChild(menu);
-    document.getElementById('cmt-btn').addEventListener('click', function() {
-        let comment = this.previousSibling.previousSibling.value;
-        let addedComment = document.createElement('div');
-        addedComment.innerHTML = (`<div id="new-comment"> ${comment}
-        </div>`);
-        document.body.appendChild(addedComment);
-        console.log(addedComment);
-        document.getElementById('comment-menu').remove();
-    });
+    </div>`;
+  document.body.appendChild(menu);
+  document.getElementById('cmt-btn').addEventListener('click', function () {
+    let comment = this.previousSibling.previousSibling.value;
+    let addedComment = document.createElement('div');
+    addedComment.innerHTML = `<div id="new-comment"> ${comment}
+        </div>`;
+    document.body.appendChild(addedComment);
+    console.log(addedComment);
+    document.getElementById('comment-menu').remove();
+  });
 }
 document.body.addEventListener('contextmenu', comment);
 // document.addEventListener('mouseup', function() {
@@ -26,5 +27,4 @@ document.body.addEventListener('contextmenu', comment);
 //     highlighted.focusNode.style.backgroundColor = 'yellowgreen';
 // });
 
-
-console.log(document)
+console.log(document);
