9fca5b16545401994d0b28549b302fae7e75512d
AP <ap@Allisons-MBP.cable.rcn.com>
2020-04-16 00:07:19 -0400

    12:06

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
 
-  chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
+    chrome.runtime.onMessage.addListener(
+        function(message, callback) {
+          if (message == 'runContentScript') {
+            chrome.tabs.executeScript({
+              file: 'contentScript.js'
+            });
+          }
+       });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
@@ -13,8 +17,9 @@
         }]);
       });
 
+
+
     console.log(window.document);
     // document.querySelector('#button').addEventListener('click', function() {
     //     console.log(this);
     // })
-  });
--- popup.js
+++ popup.js
@@ -1,8 +1,49 @@
-let login = document.getElementById('login');
-console.log(document.body);
+var db = new PouchDB('http://134.122.118.232:5984/hackathon');
+console.log(db);
+
+class User {
+    constructor(name, id) {
+        this._id = id;
+        this.name = name;
+    }
+}
+
+
+const login = document.getElementById('login');
+const addPage = document.getElementById('addpage');
+addPage.onclick = function(element) {
+  const day = new Date().getDay
+  const doc = {_id: Date.now(), day: day.getDate(), url: 'http://nytimes.com'}
+  db.put(docs, function(err, result) {
+    if (!err) console.log('added');
+    else throw(err);
+})
+}
+
+
+const data = {
+  "_id": "mittens",
+  "name": "Mittens",
+  "occupation": "kitten",
+  "age": 3,
+  "hobbies": [
+    "playing with balls of yarn",
+    "chasing laser pointers",
+    "lookin' hella cute"
+  ]
+}
+
 
 
 login.onclick = function(element) {
-    console.log(element);
+    console.log(window.document);
+    let username = document.getElementById('username-field').value;
+    console.log(username);
+    const docs = new User(username, "1");
+    console.log(JSON.stringify(docs));
+    db.put(docs, function(err, result) {
+        if (!err) console.log('added');
+        else throw(err);
+    })
 
   };
